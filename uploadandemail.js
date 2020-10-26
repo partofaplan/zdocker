00bd7d61d822c55944b008d1324aeea1a376096d
Zach Perkins <partofaplan@gmail.com>
2020-10-26 13:13:09 -0500

    Trying to remove textbox with submit button

--- uploadandemail.js
+++ uploadandemail.js
@@ -14,7 +14,7 @@ var transporter = nodemailer.createTransport({
 var srvr = http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
-        form.parse(req, function (err, files) {
+        form.parse(req, function (err, fields, files) {
             var oldpath = files.filetoupload.path;
             var dir = './uploads/';
             if (!fs.existsSync(dir)) {
@@ -44,7 +44,7 @@ var srvr = http.createServer(function (req, res) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
         res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submite">');
+        res.write('<input type="submit">');
         res.write('</form>');
         return res.end;
     }
